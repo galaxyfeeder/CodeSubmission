0369be32d3a9e72d3d755368fd0001a1a3d7b91e
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-06-06 20:13:54 +0200

    Misc

--- problem/views.py
+++ problem/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from problem.models import Problem
+
 
 class ProblemDetail(TemplateView):
     template_name = "problem/problem_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProblemDetail, self).get_context_data(**kwargs)
+
+        context['problem'] = problem = Problem.objects.get(pk=kwargs.get('pk', None))
+        context['title'] = problem.title
+
+        return context
