6fdf4d81974d94595ed21fdc5c4c50bb0a6ec385
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-06-06 17:02:33 +0200

    Log out and base changes

--- code_submission/settings.py
+++ code_submission/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ca-es'
 
 TIME_ZONE = 'UTC'
 
