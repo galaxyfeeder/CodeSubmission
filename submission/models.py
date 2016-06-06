334395e37ceb1c1e578cca66815239170c090629
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-06-06 18:14:03 +0200

    Submission

---
+++ submission/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+import urlparse
+
+from django.core.files.storage import FileSystemStorage
+from django.db import models
+
+from code_submission.settings import BASE_DIR
+from problem.models import Problem
+from ums.models import Student
+
+
+class Submission(models.Model):
+    problem = models.ForeignKey(Problem)
+    submitter = models.ForeignKey(Student)
+    code = models.FileField(storage=FileSystemStorage(location=urlparse.urljoin(BASE_DIR, 'static_server/protected')),
+                            upload_to='submissions')
