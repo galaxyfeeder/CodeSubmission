1856de01ecf5e20a5eb0898fd3799aa914d33ee1
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-06-06 20:00:23 +0200

    Added order

--- problem/models.py
+++ problem/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Problem(models.Model):
     title = models.CharField(max_length=140)
     body = models.TextField()
+    order = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
 
 
 class TestCase(models.Model):
