131a025931111e594687763c5c3f5255b4fe45f1
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-06-07 11:39:23 +0200

    Submission list and submission detail

--- submission/urls.py
+++ submission/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from submission.views import SubmitView
+from submission.views import SubmitView, SubmissionDetail, SubmissionList
 
 urlpatterns = [
-    url(r'^problem/(?P<pk>\d+)/submit$', login_required(SubmitView.as_view()), name='submit')
+    url(r'^problem/(?P<pk>\d+)/submit$', login_required(SubmitView.as_view()), name='submit'),
+    url(r'^problem/(?P<pk>\d+)/submissions$', login_required(SubmissionList.as_view()), name='submissions'),
+    url(r'^problem/(?P<pk>\d+)/submissions/(?P<pk_submission>\d+)$', login_required(SubmissionDetail.as_view()), name='submission'),
+    url(r'^problem/(?P<pk>\d+)/submissions/(?P<pk_submission>\d+)/code$', login_required('submission.views.serve_submission_file'), name='code')
 ]
--- submission/views.py
+++ submission/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
@@ -22,7 +23,42 @@ class SubmitView(TemplateView):
         form = SubmissionForm(request.POST, request.FILES)
 
         if form.is_valid():
-            submission = Submission(problem=problem, submitter=request.user.student, code=request.FILES['code'])
+            submission = Submission(problem=problem, submitter=request.user.student, code=request.FILES['code'],
+                                    number=Submission.objects.filter(problem=problem,
+                                                                     submitter=request.user.student).count()
+                                    )
             submission.save()
 
         return redirect('submit', problem.pk)
+
+
+def serve_submission_file(request, *args, **kwargs):
+    submission = Submission.objects.get(pk=kwargs.get('pk_submission', None))
+    fsock = open(submission.code.path, 'r')
+    response = HttpResponse(fsock)
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment; filename=submission-' + str(submission.pk)
+    return response
+
+
+class SubmissionDetail(TemplateView):
+    template_name = "submission/submission_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SubmissionDetail, self).get_context_data(**kwargs)
+
+        context['submission'] = Submission.objects.get(pk=kwargs.get('pk_submission', None))
+
+        return context
+
+
+class SubmissionList(TemplateView):
+    template_name = "submission/submission_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SubmissionList, self).get_context_data(**kwargs)
+
+        context['submissions'] = Submission.objects.filter(submitter=self.request.user.student)
+        context['problem'] = Problem.objects.get(pk=kwargs.get('pk', None))
+
+        return context
