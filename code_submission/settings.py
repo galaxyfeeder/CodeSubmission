e52b73b202fa2c121dee1760a0b9c2346ff87b17
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-06-07 15:51:56 +0200

    Misc

--- code_submission/settings.py
+++ code_submission/settings.py
@@ -85,8 +85,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
