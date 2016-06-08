cd2e63baaa5cf8241c6809ea08007cf0191cd24a
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-06-08 15:43:50 +0200

    Misc

--- code_submission/settings.py
+++ code_submission/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
+DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
